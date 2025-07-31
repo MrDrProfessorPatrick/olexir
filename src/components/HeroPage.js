268a5e26896f4e5db2759eb52752d776061725ff
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-07-31 20:07:16 +0300

    css transtion added to herPage

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -12,12 +12,12 @@ export default function HeroPage() {
         sizes="1000px"
       />
       <div className="absolute inset-0 flex flex-col items-center justify-center top-[-180px]">
-        <div className="p-0 m-0">
+        <div className="p-0 m-0 animate-slideDown">
           <h1 className="lg:text-[200px] md:text-[140px] text-[62px] font-bold text-black leading-[10rem]">
             Olexir
           </h1>
         </div>
-        <div className="md:text-[47px] text-[27px] text-white text-center bg-black w-full pt-[20px] pb-[20px]">
+        <div className="md:text-[47px] text-[27px] text-white text-center bg-black w-full pt-[20px] pb-[20px] animate-slideLeft">
           Versatile Platform for Lipid Engineering
         </div>
       </div>
