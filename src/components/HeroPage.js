f7148bb9b198fb4075a24935cf09d89b4c602a68
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-08-07 23:27:41 +0300

    MenuButton added

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import MenuButtons from "./MenuButtons";
 
 export default function HeroPage() {
   return (
@@ -11,19 +12,8 @@ export default function HeroPage() {
         priority
         sizes="(max-width: 200px)"
       />
-      <div className="absolute right-15 mt-6 pb-4 pt-4 flex flex-row items-center">
-        <button className="bg-black rounded-2xl pt-2 pb-2 pl-4 pr-4 mr-5">
-          Home
-        </button>
-        <button className="bg-black rounded-2xl pt-2 pb-2 pl-4 pr-4 mr-5">
-          Technology
-        </button>
-        <button className="bg-black rounded-2xl pt-2 pb-2 pl-4 pr-4 mr-5">
-          Team
-        </button>
-        <button className="bg-black rounded-2xl pt-2 pb-2 pl-4 pr-4 mr-5">
-          Amout
-        </button>
+      <div className="absolute right-15 mt-6 pb-4 pt-4 flex flex-row items-center z-1000">
+        <MenuButtons buttons={["Home", "Technology", "Team", "About"]} />
       </div>
       <div className="absolute inset-0 flex flex-col items-center justify-center">
         <div className="md:text-[37px] text-[17px] text-white text-center bg-black w-full animate-slideLeft">
---
+++ src/components/MenuButtons.js
@@ -0,0 +1,39 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function MenuButtons({ buttons }) {
+  return (
+    <div className="absolute flex gap-10 right-15 mt-10 pb-4 pt-4 flex flex-row items-center">
+      {buttons.map((button) => (
+        <motion.button
+          key={button}
+          initial={{ "--x": "100%", scale: 1 }}
+          animate={{ "--x": "-100%" }}
+          whileTap={{ scale: 0.97 }}
+          transition={{
+            repeat: Infinity,
+            repeatType: "loop",
+            repeatDelay: 1,
+            type: "spring",
+            stiffness: 20,
+            damping: 15,
+            mass: 2,
+            scale: {
+              type: "spring",
+              stiffness: 10,
+              damping: 5,
+              mass: 0.1,
+            },
+          }}
+          className="px-6 py-2 rounded-xl relative cursor-pointer radial-gradient"
+        >
+          <span className="text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask ">
+            {button}
+          </span>
+          <span className="pointer-events-none block absolute inset-0 rounded-xl p-px linear-overlay" />
+        </motion.button>
+      ))}
+    </div>
+  );
+}
