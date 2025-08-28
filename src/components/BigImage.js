d682dff72a8db06cd2bc3571e7ead537ef21d30b
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-08-28 16:13:42 +0300

    div with text added to BigImage

--- src/components/BigImage.js
+++ src/components/BigImage.js
@@ -21,13 +21,14 @@ export default function BigImage({
           inset: 0,
           background: "rgba(0, 0, 0, 0.8)",
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           zIndex: 999,
         }}
       >
         <motion.div
-            className="relative w-[700px] h-[700px] cursor-pointer rounded-lg overflow-hidden"
+            className="relative w-[600px] rounded-t-xl h-[600px] cursor-pointer overflow-hidden"
             key={bigImageIndex}
             layoutId={`expandable-image-${bigImageIndex}`}
             initial={{ opacity: 0 }}
@@ -40,8 +41,21 @@ export default function BigImage({
           <motion.img
             src={src}
             alt="Expanded"
-             className="w-[700px] h-[700px] rounded-lg object-contain"
+             className="w-[700px] h-[700px] object-contain"
           />
+        </motion.div>
+        <motion.div 
+            className="bg-white w-[600px] p-6 rounded-b-xl"
+            initial={{ opacity: 0, scaleY: 0, transformOrigin: "top" }}
+            animate={{ opacity: 1, scaleY: 1 }}
+            exit={{ opacity: 0, scaleY: 0 }}
+            transition={{
+              duration: 0.3,
+              ease: [0.4, 0, 0.2, 1],
+              delay: 0.4,
+      }}
+        >
+          THIS DIV SHOULD EXPAND HERE
         </motion.div>
       </motion.div>
     </AnimatePresence>
