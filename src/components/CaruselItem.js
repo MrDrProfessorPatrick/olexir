43989d159e48fb8ffcd2c3394ed47c16ad22a2c5
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-08-28 12:49:31 +0300

    ugly image return to carusel fixed

--- src/components/CaruselItem.js
+++ src/components/CaruselItem.js
@@ -21,16 +21,16 @@ const CarouselItem = React.memo(function CarouselItem({
       key={index}
       className="flex-[0_0_100%] 2xl:flex-[0_0_33.333%] xl:flex-[0_0_50%] lg:flex-[0_0_50%] md:flex-[0_0_100%] sm:flex-[0_0_100%] flex items-center justify-center content-center p-4"
       onClick={() => itemClick(index, src)}
+      layoutId={`expandable-image-${index}`} // animation problem is here
+      transition={{
+        layout: { duration: 0.8, ease: [0.4, 0, 0.2, 1] },
+      }}
     >
       <div className="relative w-[250px] h-[250px] 2xl:w-[500px] xl:w-[500px] lg:w-[500px] md:w-[500px] 2xl:w-[500px] xl:h-[500px] lg:h-[500px] md:h-[500px] sm:w-[400px] sm:h-[400px] cursor-pointer rounded-lg overflow-hidden">
         {imageIndex === null && (
           <motion.img
             src={src}
             key={index}
-            layoutId={`expandable-image-${index}`} // animation problem is here
-            transition={{
-              layout: { duration: 0.8, ease: [0.4, 0, 0.2, 1] },
-            }}
             alt={`Carousel image ${imageIndex}`}
             className="object-cover w-full h-full"
           />
