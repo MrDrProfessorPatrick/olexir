719a9060db26c7cf9feac14bf3ecee2b69eba3c1
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-09-18 13:53:27 +0300

    VideoBlock added

--- src/app/technology/page.js
+++ src/app/technology/page.js
@@ -1,8 +1,23 @@
 import BgVideo from "@/components/BgVideo";
-import PageInDevelopmentPlaceholder from "@/components/PageInDevelopmentPlaceholder";
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+  variable: "--font-lato",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
 
 export default function Solution() {
   return (
-    <PageInDevelopmentPlaceholder/>
+    <>
+      <div className="h-[620px]">
+        <BgVideo link={"/solutionVideo.mp4"} />
+        <div className="absolute top-116  z-20 w-full flex justify-center">
+          <h1 className={`text-[56px] font-bold text-white ${lato.className}`}>
+            World needs efficient, healthy and sustainable fats
+          </h1>
+        </div>
+      </div>
+    </>
   );
 }
