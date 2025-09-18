dae1d4eed4d59718214ac734346c104d57aaba31
MrDrProfessorPatrick <vip.salm2@gmail.com>
2025-09-18 16:53:54 +0300

    18Sep16.53

--- src/app/technology/page.js
+++ src/app/technology/page.js
@@ -10,12 +10,48 @@ const lato = Lato({
 export default function Solution() {
   return (
     <>
-      <div className="h-[620px]">
+      <div className="h-[620px] bg-grey">
         <BgVideo link={"/solutionVideo.mp4"} />
         <div className="absolute top-116  z-20 w-full flex justify-center">
           <h1 className={`text-[56px] font-bold text-white ${lato.className}`}>
             World needs efficient, healthy and sustainable fats
           </h1>
+        </div>
+        <div>
+          <div className="relative w-full px-10">
+            <div className="pt-20 pb-12 h-[115px]">
+              <h3 className="">Problem</h3>
+            </div>
+            <div className="flex">
+              <div>
+                <div>#1</div>
+                <div>
+                  <p>
+                    Heart attack and stroke, triggered by saturated fats, are
+                    the #1 killer in the world
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div>60%</div>
+                <div>
+                  <span>
+                    Weight is a global burden: 43% overweight + 16% obesity in
+                    2022
+                  </span>
+                </div>
+              </div>
+              <div>
+                <div>5%</div>
+                <div>
+                  <span>
+                    adsorption of lipophilic bioactives, e.g. curcumin and CBD,
+                    by the human body
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
