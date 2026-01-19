c311835103f8d33aff7ad376087d0f45f3d588c0
MrDrProfessorPatrick <vip.salm2@gmail.com>
2026-01-20 00:30:20 +0200

    Home page.tsx done

---
+++ src/components/LineCircleLeft.tsx
@@ -0,0 +1,24 @@
+interface LineCircleLeftProps {
+    top: number
+    width: number
+    rightEdge?: string
+}
+
+export default function LineCircleLeft({
+    top,
+    width,
+    rightEdge,
+}: LineCircleLeftProps) {
+    return (
+        <div
+            style={{ top: `${top}%`, right: rightEdge || '0' }}
+            className={`hidden md:flex items-center hidden absolute z-1000`}
+        >
+            <div className="w-1 h-1 border-2 border-white bg-white rounded-full flex items-center justify-center"></div>
+            <div
+                style={{ width: `${width}px` }}
+                className="h-[1px] bg-white"
+            ></div>
+        </div>
+    )
+}
