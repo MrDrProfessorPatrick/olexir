5cfb86245bd55d01d7eb1a0aeff2f40d95a7b44c
MrDrProfessorPatrick <vip.salm2@gmail.com>
2026-01-26 12:27:03 +0200

    BlogEditor added

--- src/app/(site)/blog/page.tsx
+++ src/app/(site)/blog/page.tsx
@@ -1,12 +1,20 @@
 import { auth } from '@clerk/nextjs/server'
+import BlogEditor from '../../../components/BlogEditor'
 
 export default async function Blog() {
-    const { userId, sessionStatus } = await auth()
+    const { userId } = await auth()
 
-    console.log('USER ID ON ROOT', userId, 'SESSION STATUS', sessionStatus)
+    if (!userId) {
+        return (
+            <div className="min-h-screen bg-black text-white flex items-center justify-center">
+                BLOG CONTENT
+            </div>
+        )
+    }
+
     return (
-        <div className="min-h-screen bg-black flex items-center justify-center">
-            BLOG
+        <div className="min-h-screen bg-black text-white p-10">
+            <BlogEditor />
         </div>
     )
 }
