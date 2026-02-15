ae5d12f80579167dfba104c151b67fc36714ffd4
MrDrProfessorPatrick <vip.salm2@gmail.com>
2026-02-15 02:16:29 +0200

    transparent navbar

--- src/components/MenuButtons.js
+++ src/components/MenuButtons.js
@@ -16,7 +16,7 @@ export default function MenuButtons({ buttons }) {
     const activeButton = getActiveButton()
 
     return (
-        <div className="absolute flex gap-10 right-1 2xl:right-10 xl:right-10 lg:right-10 md:right-10 sm:gap-2 sm:right-1 mt-6 pb-4 pt-4 flex flex-row items-center">
+        <div className="absolute flex gap-10 right-1 2xl:right-10 xl:right-10 lg:right-10 md:right-10 sm:gap-2 sm:right-1 mt-5 pb-4 pt-4 flex flex-row items-center">
             {buttons.map((button) => {
                 const isActive = activeButton === button
                 const href = `/${button.replace(/\s+/g, '').toLowerCase()}`
@@ -25,15 +25,11 @@ export default function MenuButtons({ buttons }) {
                         <button
                             type="button"
                             key={button}
-                            className={`px-2 lg:px-6 py-2 lg:py-4 rounded-xl relative cursor-pointer z-[1000] ${
-                                isActive ? 'bg-[#D8AE02]' : 'bg-[#231428]'
-                            }`}
+                            className={`relative cursor-pointer z-[1000]`}
                         >
                             <span
-                                className={`tracking-wide px-4 block relative linear-mask lg:text-xl ${
-                                    isActive
-                                        ? 'text-black'
-                                        : 'text-neutral-100 '
+                                className={`tracking-wide px-4 block relative linear-mask lg:text-xl font-bold ${
+                                    isActive ? 'text-neutral-100' : 'text-black'
                                 }`}
                             >
                                 {button}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,8 +7,8 @@ export default function NavBar() {
     const buttons = ['Technology', 'Applications', 'About Us']
 
     return (
-        <div className="absolute w-full h-[100px]">
-            <div className="absolute inset-0 flex items-center justify-center z-10 w-[180px] h-[60px] xs:w-[180px] xs:h-[60px] lg:w-[220px] lg:h-[70px] ml-[5%] mt-2 md:mt-6 lg:mt-7 z-1000">
+        <div className="absolute w-full h-[100px] backdrop-blur-md bg-transparent z-50 ">
+            <div className="absolute inset-0 flex items-center justify-center z-10 w-[180px] h-[60px] xs:w-[180px] xs:h-[60px] lg:w-[220px] lg:h-[70px] ml-[5%] mt-4 z-1000">
                 <Link
                     href="/"
                     className="absolute inset-0 z-20"
